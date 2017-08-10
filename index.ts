580c1ebc5e73a8e7ed8fb86cdf98a35ba20e2ebe
Bernard Rodrigue <bernard.rodrigue+github@gmail.com>
2017-08-09 22:14:21 -0400

    Using async, fix issues

--- index.ts
+++ index.ts
@@ -1,37 +1,62 @@
 var exif = require('fast-exif');
+var mkdirp = require('mkdirp');
 
 import * as fs from 'fs';
 import * as path from 'path';
 import * as minimist from 'minimist';
+import * as async from 'async';
 
 var argv = minimist(process.argv.slice(2));
 console.dir(argv);
 
-const sourceDir = 'D:\\temp\\images';
-const targetDir = 'D:\\temp\\images_out';
+const sourceDir = argv.source;
+const destinationRootDir = argv.destination;
+
+interface ExifData{
+    exif:{
+        DateTimeOriginal:Date;
+    }
+}
 
 fs.readdir(sourceDir, function(err, files) {
      
-    files.forEach(file => {
-        let full = path.join(sourceDir,file);
-        exif.read(full).then(function(data:any){
-            //console.log(data);
-            var dateTaken = new Date(data.exif.DateTimeOriginal);
-            
-            var destination = path.join(
-                targetDir, 
-                dateTaken.getFullYear().toString(), 
-                ("00" + dateTaken.getMonth()).slice(-2),
-                ("00" + dateTaken.getDate()).slice(-2),
-                file
-            );
-            
-            console.log(destination);
+    async.mapLimit(files, 8, function(file, done){
 
-            // todo: make dir
+        var src = path.join(sourceDir,file);
+        
+        var operations = {
+            get_destination: function(done:any){
+                exif.read(src).then(function(data:ExifData){
 
-            fs.rename(full,destination,err=>console.log);
-        });     
+                    var dateTaken = data.exif.DateTimeOriginal;
+                    
+                    var year = dateTaken.getFullYear().toString();
+                    var month = ("00" + dateTaken.getMonth()+1).slice(-2);
+                    var day = ("00" + dateTaken.getDate()).slice(-2);
+                    
+                    var destinationDir = path.join(
+                        destinationRootDir, 
+                        `${year}`, 
+                        `${year}-${month}`,
+                        `${year}-${month}-${day}`, 
+                    );  
+
+                    done(null, destinationDir);
+                }).catch(function(err:any){done(err)});
+            },
+            prepare_destination: ['get_destination', function(results:any, done:any){
+                mkdirp(results.get_destination, done);
+            }],
+            move_file: ['prepare_destination', function(results:any, done: any){
+                var dst = path.join(results.get_destination,file);
+                console.log(`copy ${src} to ${dst}`);
+                fs.rename(src,dst,done);                
+            }]};
+        
+        async.auto(operations, undefined, done);
+
+    }, function(err){
+       
     });
 });
 
