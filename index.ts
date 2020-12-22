8e8824118c11c1425aae500d57a470d4a38b38dd
Bernard Rodrigue <bernard.rodrigue+github@gmail.com>
2020-12-22 12:48:12 -0500

    Updated with async await

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ var exif = require('fast-exif');
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as program from 'commander';
-import * as async from 'async';
 import * as recursiveReadDir from 'recursive-readdir';
+
+import exifr from 'exifr'
 
 // Parse arguments
 program
@@ -22,6 +23,12 @@ program
     .action(function (source, destination) {
         execute(fs.move, source, destination);
     });
+
+program
+    .command('dry <source> <destination>')
+    .action(function (source, destination) {
+        execute(function move(src: string, dest: string){}, source, destination);
+    });    
     
 program.parse(process.argv);
 
@@ -32,86 +39,74 @@ interface ExifData{
     }
 }
 
-function execute(operation:Function, source:string, destination:string){
+async function execute(operation:Function, source:string, destination:string){
 
-    var mainTasks = {
-        read_existing : function(done:any){
-            recursiveReadDir(destination, ["*.jpg"], done);
-        },
-        read_source : ['read_existing', function(results: any, done:any){
-            fs.readdir(source, function (err, files) {
+    try{
+        await fs.ensureDir(destination);
+        var existingFiles = await recursiveReadDir(destination, ["*.jpg"]);
 
-                if (err) {
-                    console.log(`could not read directory. Error: ${err}`);
-                    return;
+        // Store the existing files in a set
+        var existingSet = new Set<string>();
+        existingFiles.forEach((f:string)=>existingSet.add(path.basename(f)));
+
+        var sourceFiles = await fs.readdir(source);
+
+        // Only keep the non existing files
+        sourceFiles = sourceFiles.filter(function(file){
+            return !existingSet.has(path.basename(file));
+        });
+
+        for (const file of sourceFiles) {
+            try{            
+                var src = path.join(source, file);
+
+                // Read exif
+                //var exifData:ExifData = await exif.read(src);
+                var exif = await exifr.parse(src);
+
+                var fstats = await fs.stat(src);
+                var dateTaken = fstats.birthtime;
+                if(exif){
+                    dateTaken = exif.DateTimeOriginal;
                 }
 
-                // Store the existing files in a set
-                var existing = new Set<string>();
-                results.read_existing.forEach((f:string)=>existing.add(path.basename(f)));
+                var year = dateTaken.getFullYear().toString();
+                var month = ("00" + (dateTaken.getMonth() + 1)).slice(-2);
+                var day = ("00" + dateTaken.getDate()).slice(-2);
 
-                // Only keep the non existing files
-                files = files.filter(function(file){
-                    return !existing.has(path.basename(file));
-                });
+                var destinationDir = path.join(
+                    destination,
+                    `${year}`,
+                    `${year}-${month}`,
+                    `${year}-${month}-${day}`,
+                );
+                
+                // Prepare destination
+                await fs.ensureDir(destinationDir);
 
-                // Process files
-                async.mapLimit(files, 8, function (file, done) {
+                var dst = path.join(destinationDir, file);
+                console.log(`${operation == fs.copy ? 'copy' : 'move'} ${file} to ${destinationDir}`);
 
-                    var src = path.join(source, file);
-
-                    var operations = {
-                        get_destination: function (done: any) {
-                            exif.read(src).then(function (data: ExifData) {
-
-                                var dateTaken = data.exif.DateTimeOriginal;
-
-                                var year = dateTaken.getFullYear().toString();
-                                var month = ("00" + (dateTaken.getMonth() + 1)).slice(-2);
-                                var day = ("00" + dateTaken.getDate()).slice(-2);
-
-                                var destinationDir = path.join(
-                                    destination,
-                                    `${year}`,
-                                    `${year}-${month}`,
-                                    `${year}-${month}-${day}`,
-                                );
-
-                                done(null, destinationDir);
-                            }).catch(function (err: any) { done(err) });
-                        },
-                        prepare_destination: ['get_destination', function (results: any, done: any) {
-                            fs.ensureDir(results.get_destination, done);
-                        }],
-                        move_file: ['prepare_destination', function (results: any, done: any) {
-                            var dst = path.join(results.get_destination, file);
-                            console.log(`${operation == fs.copy ? 'copy' : 'move'} ${src} to ${dst}`);
-
-                            operation(src, dst, done);
-                        }]
-                    };
-
-                    // Execute file operations
-                    async.auto(operations, undefined, done);
-
-                }, done);
-            });            
-        }]
-    };
-    
-    async.auto(mainTasks, undefined,
-        function (err, results: any[]) {
-
-            if (err) {
-                console.log(`Did not complete the operation. Error: ${err}`);
-                return;
+                // Execute operation
+                await operation(src, dst);
             }
+            catch(e)
+            {
+                console.error(`Error while processing file ${file}: ${e}`);
+            }
+        }
 
-            console.log(`Successfully completed the operation.`);
-            console.log('Press any key to exit');
+        console.log(`Successfully completed the operation.`);
 
-            //process.stdin.setRawMode(true);
-            process.stdin.resume();
-            process.stdin.on('data', process.exit.bind(process, 0));            
-        });
+    }
+    catch(e)
+    {
+        console.error(`Did not complete the operation. Error: ${e}`);
+    }
+    
+    console.log('Press any key to exit');
+
+    //process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));            
 }
